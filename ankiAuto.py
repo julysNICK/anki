e8d9a320656b123eaed289082df0adf6bb280956
julysNICK <julysmartins54@gmail.com>
2023-02-05 19:44:17 -0300

    fix ankiAuto

--- ankiAuto.py
+++ ankiAuto.py
@@ -7,6 +7,15 @@ words = []
 exitProgram = False
 
 
+def formatSetence(word):
+    word = word.strip()
+    word = word.split()
+    finalText = " ".join(word)
+    finalText = finalText.replace(" ", "%20")
+
+    return finalText
+
+
 def sentenceOrWord(word):
     if " " in word:
         return "sentence"
@@ -19,8 +28,8 @@ def translate(word):
 
     driver = webdriver.Firefox()
     driver.get(
-        "https://translate.google.com/#view=home&op=translate&sl=en&tl=pt&text=" + word)
-    time.sleep(2)
+        "https://translate.google.com/#view=home&op=translate&sl=en&tl=pt&text=" + formatSetence(word))
+    time.sleep(5)
 
     try:
         if setence_word == "word":
@@ -31,10 +40,12 @@ def translate(word):
             print("sentence")
             translation = driver.find_element_by_xpath(
                 '/html/body/c-wiz/div/div[2]/c-wiz/div[2]/c-wiz/div[1]/div[2]/div[3]/c-wiz[2]/div/div[8]/div[1]/div[1]/span[1]').text
+
     except NoSuchElementException:
+        print("word")
         translation = driver.find_element_by_xpath(
             '/html/body/c-wiz/div/div[2]/c-wiz/div[2]/c-wiz/div[1]/div[2]/div[3]/c-wiz[2]/div/div[8]/div[1]/div[1]/span[1]').text
-
+        driver.quit()
     driver.quit()
     return translation
 
@@ -42,14 +53,12 @@ def translate(word):
 def add_to_csv():
     try:
         with open('words.csv', 'a', newline='') as csvfile:
-            spamwriter = csv.writer(csvfile, delimiter=' ',
-                                    quotechar='|', quoting=csv.QUOTE_MINIMAL)
+            spamwriter = csv.writer(csvfile)
             for word in words:
                 spamwriter.writerow([word[0], word[1]])
     except FileNotFoundError:
         with open('words.csv', 'w', newline='') as csvfile:
-            spamwriter = csv.writer(csvfile, delimiter=' ',
-                                    quotechar='|', quoting=csv.QUOTE_MINIMAL)
+            spamwriter = csv.writer(csvfile)
             for word in words:
                 spamwriter.writerow([word[0], word[1]])
 
@@ -58,9 +67,11 @@ while exitProgram == False:
     word = input("Enter a word to translate: ")
     if word == "exit":
         exitProgram = True
-        add_to_csv()
+
     else:
         translation = translate(word)
         print(translation)
         words.append([word, translation])
+        add_to_csv()
+        words.clear()
         print(words)
